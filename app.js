14ebdaddd41a603ea02812873f07d98d485d711e
Rafał Ostrowidzki <r.ostrowidzki@gmail.com>
2021-01-06 21:42:11 +0100

    Replace credentials.js with .env

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
-const credentials = require('./credentials');
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-mongoose.connect(credentials.dbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DB_ACCESS_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-// Encryption
-var secret = "fwefegdfgsdgdfhrjrhdbsdhfghfs";
-userSchema.plugin(encrypt, { secret: secret , encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password'] });
 
 const User = mongoose.model('User', userSchema);
 
